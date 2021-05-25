70360e3f086e8135da12c43e1c5c04a0bb47b510
android_sushi <77870989+android-sushi@users.noreply.github.com>
2021-05-25 23:24:52 +0900

    fix html and urls.py

--- we_bought_again/urls.py
+++ we_bought_again/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 app_name = 'we_bought_again'
 urlpatterns = [
-    path('we-bought-again', views.IndexView.as_view(), name='index'),
+    path('', views.IndexView.as_view(), name='index'),
 ]
